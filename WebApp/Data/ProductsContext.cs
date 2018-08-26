5db95511adb6ccb9fd062f44675368f9da3c5f03
Houssem <houssem@Houssem.local>
2018-08-26 11:37:40 +0200

    first commit

---
+++ WebApp/Data/ProductsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext (DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<WebApp.Models.Product> Product { get; set; }
+    }
+}
